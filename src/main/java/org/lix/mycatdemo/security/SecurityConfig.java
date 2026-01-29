2adfa3663d5225badfd85b12f092e9cb49f2c30d
11188813 <2809892243@qq.com>
2026-01-29 10:14:05 +0800

    spring-security-init 0.0.1

--- src/main/java/org/lix/mycatdemo/security/SecurityConfig.java
+++ src/main/java/org/lix/mycatdemo/security/SecurityConfig.java
@@ -1,8 +1,10 @@
 package org.lix.mycatdemo.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SecurityConfig {
+
 
 }
